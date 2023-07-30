d82fcbb30f2b7296e2988c5454ac6e6443415796
lalaibrahimzade <lalaibrahimzade19@gmail.com>
2023-07-30 16:38:28 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let counter = document.getElementById("counter")
+let count = 0
+let saveInfo = document.getElementById("save-res")
+
+function increment() {
+    count += 1
+    counter.textContent = count
+}
+
+function saver() {
+    let saverString =  count + " - "
+    saveInfo.textContent += saverString
+    counter.textContentv = 0
+    count = 0
+}
+
+
+
